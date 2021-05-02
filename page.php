626ded105525eecf2186e0b02df4961540f865a7
Ruben.M <rmarin@miva.com>
2021-05-01 22:05:37 -0700

    added helpers sass file, started hero block, corrected variables name was 'varibles'

--- page.php
+++ page.php
@@ -4,9 +4,14 @@
 	<?php while ( have_posts() ) : the_post(); ?>
 		<?php the_content(); ?>
 
-		<?php get_template_part( 'tmpl/quote', 'background' ); ?>
+		<?php //get_template_part( 'tmpl/quote', 'background' ); ?>
 
-		<a href="#" class="btn">Button Text</a>
+		<!-- <a href="#" class="btn">Button Text</a> -->
+
+
+	<!-- storefront -->
+	<?php get_template_part( 'tmpl/blocks/hero' ); ?>
+	<!--  -->
 
 	<?php endwhile; ?>
 <?php endif; ?>
---
+++ tmpl/blocks/hero.php
@@ -0,0 +1,17 @@
+<!-- 
+	note: will need to add responsive images for backgrounds
+ -->
+ 
+<section class="block-hero has-background" style="background-image:url(<?php echo get_template_directory_uri(  );?>/images/hero-1-mobile.jpg);">
+	<div class="layout-container">
+		<div class="block-hero-content color-white text-center">
+			<h1 class="is-heading">Custom Vision Correction, <span>Tailored to Your Needs</span></h1>
+			<div class="block-hero-text">
+				<p>From cataract surgery and corneal treatments, to vision correction surgeries such as Visian ICL and LASIK, our team of expert eye doctors at San Francisco Eye Institute are committed to providing our patients with top quality eye care.</p>
+			</div>
+			<div class="block-hero-btn">
+				<a href="" class="btn">Request a Consultation</a>
+			</div>
+		</div>
+	</div>
+</section>
